46d8c212a26acc79dad74b1edacd290a968de8bd
Akash <akashpje@users.noreply.github.com>
2023-11-26 10:49:08 +0530

    test

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def home():
     return '<h1> Test message: This app has now been deployed on AWS EC2 using github actions </h1>'
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0',  port=5000)
+    app.run(host='0.0.0.0', port=5000)
